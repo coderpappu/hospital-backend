12892da44e8492ff6fa6a759cd140888a37a977b
coderpappu <pappudey.coder@gmail.com>
2024-07-28 19:20:50 +0600

    account exception error handle

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -3,8 +3,20 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin Created successfully!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error,
+    });
+  }
 };
 
 export const userController = {
