636084b1d4a815e8e461740ffac8d89f16ad110e
coderpappu <pappudey.coder@gmail.com>
2024-07-28 19:44:00 +0600

    admin data get system ready

---
+++ src/app/modules/Admin/admin.controller.ts
@@ -0,0 +1,24 @@
+import { Request } from "express";
+import { AdminServices } from "./admin.service";
+
+const getAllFromDB = async (req: Request, res: Response) => {
+  try {
+    const result = await AdminServices.getAllFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Admin data fetch fail",
+      error: error,
+    });
+  }
+};
+
+export const AdminController = {
+  getAllFromDB,
+};
